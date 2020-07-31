b81a703b262cea8f7187763cb1a5b1f36636fc29
napaDD <68295283+napaDD@users.noreply.github.com>
2020-07-31 17:24:33 +0300

    Updates

---
+++ precache-manifest.1b3f1ca5872ec77ae30a0007b005725d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14f6a3e027e996d9e4cab0f65ad8d292",
+    "url": "/react-car/index.html"
+  },
+  {
+    "revision": "484798e1ad7a7fd32bed",
+    "url": "/react-car/static/css/main.ea88ef68.chunk.css"
+  },
+  {
+    "revision": "6a7ade9cdf28a5426ec4",
+    "url": "/react-car/static/js/2.13c3efb8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-car/static/js/2.13c3efb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "484798e1ad7a7fd32bed",
+    "url": "/react-car/static/js/main.4def9c73.chunk.js"
+  },
+  {
+    "revision": "961969a427b430cb9697",
+    "url": "/react-car/static/js/runtime-main.9c8cf3a3.js"
+  }
+]);
